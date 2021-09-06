15bc856c3680dc51c49439d1240e3227b6d4520c
dev-baonqa <nguyenquanganhbao@MacBook-Air-cua-Nguyen.local>
2021-09-06 09:41:31 +0700

    update

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,37 +1,119 @@
-import { User } from '../entities/User';
+import argon2 from "argon2";
 import { Arg, Mutation, Resolver } from "type-graphql";
-import argon2 from 'argon2'
+import { User } from "../entities/User";
+import { RegisterInput } from "../types/RegisterInput";
+import { UserMutationResponse } from "../types/UserMutationResponse";
+import { validateRegisterInput } from "../utils/validationRegisterInput";
+import { LoginInput } from "../types/LoginInput";
 
 @Resolver()
 export class UserResolver {
-    @Mutation(_returns => User, {nullable: true})
-    async register(
-        @Arg('email') email: string,
-        @Arg('username') username: string,
-        @Arg('password') password: string,
+  @Mutation((_return) => UserMutationResponse)
+  async register(
+    @Arg("registerInput") registerInput: RegisterInput
+  ): Promise<UserMutationResponse> {
+    const validareRegisterInputError = validateRegisterInput(registerInput);
 
-    ){
-        try {
-            const existingUser = await User.findOne({
-                username 
-            })
-            if(existingUser) return null
-     
-            const hashedPassword = await argon2.hash(password)
-     
-            const newUser = User.create({
-                username,
-                password: hashedPassword,
-                email
-            })
+    if (validareRegisterInputError !== null)
+      return {
+        code: 400,
+        success: false,
+        ...validareRegisterInputError,
+      };
+    try {
+      const { username, email, password } = registerInput;
+      const existingUser = await User.findOne({
+        where: [{ username }, { email }],
+      });
+      if (existingUser)
+        return {
+          code: 400,
+          success: false,
+          message: "Duplicated username or email",
+          error: [
+            {
+              field: existingUser.username === username ? "username" : "email",
+              message: `${
+                existingUser.username === username ? "Username" : "Email"
+              } already taken`,
+            },
+          ],
+        };
 
-            return await User.save(newUser)
-            
-        } catch (error) {
-            console.log(error)
-            return null
-        }
+      const hashedPassword = await argon2.hash(password);
 
+      const newUser = User.create({
+        username,
+        password: hashedPassword,
+        email,
+      });
 
+      return {
+        code: 200,
+        success: true,
+        message: "User registrastion successful",
+        user: await User.save(newUser),
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        code: 500,
+        success: false,
+        message: `Internal server error ${error.message}`,
+      };
     }
+  }
+
+  @Mutation((_return) => UserMutationResponse)
+  async login(
+    @Arg("loginInput") { usernameOrEmail, password }: LoginInput
+  ): Promise<UserMutationResponse> {
+    try {
+      const existringUser = await User.findOne(
+        usernameOrEmail.includes("@")
+          ? { email: usernameOrEmail }
+          : { username: usernameOrEmail }
+      );
+
+      if (!existringUser)
+        return {
+          code: 400,
+          success: false,
+          message: "User not found",
+          error: [
+            {
+              field: "usernameOrEmail",
+              message: "Username or email incorrect",
+            },
+          ],
+        };
+
+      const passwordValid = await argon2.verify(
+        existringUser.password,
+        password
+      );
+
+      if (!passwordValid)
+        return {
+          code: 400,
+          success: false,
+          message: "Wrong password",
+          error: [{ field: "password", message: "Wrong passwrod" }],
+        };
+
+      return {
+        code: 200,
+        success: true,
+        message: "Login in successfly",
+        user: existringUser,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        code: 500,
+        success: false,
+        message: `Internal server error ${error.message}`,
+      };
+    }
+  }
 }
